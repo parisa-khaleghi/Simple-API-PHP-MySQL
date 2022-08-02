27a96c0f726e3238e6cd3acaa0133085d3e0b1f7
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2022-08-02 17:09:16 +0300

    Feat: fetch data and print as JSON

--- src/api.php
+++ src/api.php
@@ -1,6 +1,20 @@
 <?php
 $con = mysqli_connect("localhost", "root", "root", "simple_api_DB");
 if($con){
-    echo "DB connected!";
+    $query = "Select * from person";
+    $result = mysqli_query($con, $query);
+    if($result){
+        $response = array();
+        $i = 0;
+        while($row = mysqli_fetch_assoc($result)){
+             $response[$i]['id'] = $row['id'];
+             $response[$i]['name'] = $row['name'];
+             $response[$i]['age'] = $row['age'];
+             $response[$i++]['email'] = $row['email'];
+        }
+        echo json_encode($response, JSON_PRETTY_PRINT);
+    }
+} else {
+    echo "`Database connection failed!";
 }
 ?>
